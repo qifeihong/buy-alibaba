7a673e50212e774111e2e616d608932f6d7e79c5
qfh666 <qfh_work@163.com>
2022-04-23 21:33:06 +0800

    git init

--- src/main/java/com/sale/buyalibaba/controller/DemoController.java
+++ src/main/java/com/sale/buyalibaba/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.sale.buyalibaba.controller;
 
+import cn.hutool.core.date.DateUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +14,10 @@ import java.util.Date;
 @RestController
 public class DemoController {
 
-    @GetMapping("/hello")
+    @GetMapping("/getTime")
     public String hello(){
-        System.out.println(new Date());
         System.out.println("=====================return hello world=======================");
-        return "hello world" ;
+        return "当前时间:" + DateUtil.format(new Date(), "yyyy-MM-dd HH:mm:ss");
     }
 
 }
