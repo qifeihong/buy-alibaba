fb6cc34e4f9bd00da7caeabc0167ee462f7b28e8
qfh666 <qfh_work@163.com>
2022-04-24 00:54:02 +0800

    git init

--- src/main/java/com/sale/buyalibaba/controller/DemoController.java
+++ src/main/java/com/sale/buyalibaba/controller/DemoController.java
@@ -27,11 +27,11 @@ public class DemoController {
                 "\n" +
                 "<h1>颜儿,你还不睡觉!</h1>\n" +
                 "\n" +
-                "<p style=\"font-family:verdana;color:red\">\n" +
-                "这都几点了\uD83D\uDE20</p>\n" +
+                "<h1><p style=\"font-family:verdana;color:red\">\n" +
+                "这都几点了\uD83D\uDE20</p></h1>\n" +
                 "\n" +
-                "<p style=\"font-family:verdana;color:green\">\n" +
-                ""+date+"</p>\n" +
+                "<h1><p style=\"font-family:verdana;color:green\">\n" +
+                ""+date+"</p></h1>\n" +
                 "\n" +
                 "<p style=\"font-size:30px\">这是该网站的第一行代码,后续功能,不断完善,敬请期待!</p>\n" +
                 "</body>\n" +
