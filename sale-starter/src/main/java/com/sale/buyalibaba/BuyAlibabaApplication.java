c5d224fae6ff2abcf5cebe168c675ad77c3c168f
qfh666 <qfh_work@163.com>
2022-05-05 14:01:53 +0800

    git add...

--- sale-starter/src/main/java/com/sale/buyalibaba/BuyAlibabaApplication.java
+++ sale-starter/src/main/java/com/sale/buyalibaba/BuyAlibabaApplication.java
@@ -1,10 +1,12 @@
 package com.sale.buyalibaba;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableApolloConfig
 @SpringBootApplication
 @MapperScan("com.sale.buyalibaba.mapper")
 @ComponentScan(basePackages = {"com.sale.*"})
